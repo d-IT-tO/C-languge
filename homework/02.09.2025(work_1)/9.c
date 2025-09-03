480a6bcb509b545b6e44ae027f4d9ae19374abfd
d-IT-tO <bykovivan073@gmail.com>
2025-09-03 19:14:21 +0700

    Update 9.c

--- homework/02.09.2025(work_1)/9.c
+++ homework/02.09.2025(work_1)/9.c
@@ -6,11 +6,12 @@ int main()
     int seconds, minutes, hours;
     
     scanf("%d", &seconds);
-    hours = seconds / 3600;
+    hours = int seconds / 3600;
     seconds = seconds - hours * 3600;
-    minutes = seconds / 60;
+    minutes = int seconds / 60;
     seconds = seconds - minutes * 60;
-    printf("\n%d %d %d", hours, minutes, seconds);
+    printf("\n%d:%d:%d", hours, minutes, seconds);
     
     return 0;
+
 }
