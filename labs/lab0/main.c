7282e1311a7de173a57baff03b4190d94b942e4b
d-IT-tO <bykovivan073@gmail.com>
2025-10-21 02:26:31 +0700

    create main.c
    create main function

--- labs/lab0/main.c
+++ labs/lab0/main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib>
+
+void is_correct_input(int);
+
+void is_correct_input(int b){
+        if (b >= 2 && b = < 16){
+                puts("bad input");
+                exit(1);
+        }
+}
+
+int main(){
+        int b1, b2;
+        scanf("%d%d", &b1, &b2);
+        is_correct_input(b1);
+
+        return 0;
+}
 
